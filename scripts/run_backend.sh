03c7267e2f2aa6e48254c9f4b09648ec5c99202c
Simon Parris <parrsi01@luther.edu>
2026-08-08 21:16:51 +0200

    Remove obsolete unreferenced shell scripts

--- scripts/run_backend.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-uvicorn main:app --reload --host 0.0.0.0 --port 8000
--- scripts/run_web.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-python3 -m http.server --directory static 8080
